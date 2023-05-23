02e130c7502a92eb1699434c776f9fcd487036c3
Suvradip Roy <suvro467@msn.com>
2023-05-23 22:12:33 +0530

    Data model changes.

--- lib/src/data/models/character.dart
+++ lib/src/data/models/character.dart
@@ -4,10 +4,27 @@ part 'character.g.dart';
 
 @JsonSerializable()
 class Character {
+  final int id;
   final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
   final String image;
+  final String url;
+  final String created;
 
-  Character(this.name, this.image);
+  Character(
+    this.id,
+    this.name,
+    this.status,
+    this.species,
+    this.type,
+    this.gender,
+    this.image,
+    this.url,
+    this.created,
+  );
 
   factory Character.fromJson(Map<String, dynamic> json) =>
       _$CharacterFromJson(json);
--- lib/src/data/models/character.g.dart
+++ lib/src/data/models/character.g.dart
@@ -7,8 +7,15 @@ part of 'character.dart';
 // **************************************************************************
 
 Character _$CharacterFromJson(Map<String, dynamic> json) => Character(
+      json['id'] as int,
       json['name'] as String,
+      json['status'] as String,
+      json['species'] as String,
+      json['type'] as String,
+      json['gender'] as String,
       json['image'] as String,
+      json['url'] as String,
+      json['created'] as String,
     );
 
 Map<String, dynamic> _$CharacterToJson(Character instance) => <String, dynamic>{
